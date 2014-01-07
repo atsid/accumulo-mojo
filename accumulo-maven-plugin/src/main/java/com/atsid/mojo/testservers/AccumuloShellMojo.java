eb936ec7ee93d1926e90566123557f3a3c9e08f9
Jon McLean <jon.mclean@atsid.com>
2014-01-07 09:53:56 -0800

    accumulo-mojo-6 Add "shell" goal
    Added configuration parameters to the shell mojo.

--- accumulo-maven-plugin/src/main/java/com/atsid/mojo/testservers/AccumuloShellMojo.java
+++ accumulo-maven-plugin/src/main/java/com/atsid/mojo/testservers/AccumuloShellMojo.java
@@ -15,13 +15,55 @@ import org.apache.maven.plugin.MojoFailureException;
  */
 public class AccumuloShellMojo extends AbstractTestServerMojo implements MojoMXBean {
 
+	/**
+	 * Name of the Accumulo instance. Default value is "accumulo"
+	 * 
+	 * @parameter property="accumuloInstanceName" default-value="accumulo"
+	 */
+	private String accumuloInstanceName = null;
+
+	/**
+	 * Zookeeper server port. Default value is 2181.
+	 * 
+	 * @parameter property="zookeeperPort" default-value="2181"
+	 */
+	private int zookeeperPort = 0;
+
+	/**
+	 * Zookeeper host server name. Default value is "localhost."
+	 * 
+	 * @parameter property="zookeeperHost" default-value="localhost"
+	 */
+	private String zookeeperHost = null;
+
+	/**
+	 * Password for the Accumulo user "root." Default value is "password."
+	 * 
+	 * @parameter property="accumuloPassword" default-value="password"
+	 */
+	private String accumuloPassword = null;
+
+	/**
+	 * Username to use when connecting to Accumulo. Default value is "root."
+	 * 
+	 * @parameter property="accumuloUser" default-value="root"
+	 */
+	private String accumuloUser = null;
+
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
 		try {
+            getLog().info("*********************************************");
+            getLog().info("Starting Accumulo shell");
+            getLog().info("*********************************************");
+
+            String zookeeper = String.format("%s:%s", this.zookeeperHost, this.zookeeperPort);
 			Shell shell = new Shell();
-			shell.config("-u", "root", "-p", "password", "-z", "accumulo", "localhost:2181");
+			shell.config("-u", this.accumuloUser, "-p", this.accumuloPassword, "-z", this.accumuloInstanceName,
+					zookeeper);
 			shell.start();
 		} catch (IOException e) {
+            getLog().error("Error running Accumulo shell", e);
 			throw new MojoExecutionException("Error starting Accumulo shell", e);
 		}
 	}
