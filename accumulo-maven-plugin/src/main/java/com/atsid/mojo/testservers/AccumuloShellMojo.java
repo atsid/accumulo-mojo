1ae73c4fce286da8ba7cfe3c0b85e90e711541f5
Jon McLean <jon.mclean@atsid.com>
2014-01-07 09:08:05 -0800

    accumulo-mojo-6 Add "shell" goal
    Goal works with default values.  Now to make it configurable.

--- accumulo-maven-plugin/src/main/java/com/atsid/mojo/testservers/AccumuloShellMojo.java
+++ accumulo-maven-plugin/src/main/java/com/atsid/mojo/testservers/AccumuloShellMojo.java
@@ -1,11 +1,7 @@
 package com.atsid.mojo.testservers;
 
 import java.io.IOException;
-import java.util.List;
 
-import org.apache.accumulo.core.client.AccumuloException;
-import org.apache.accumulo.core.client.AccumuloSecurityException;
-import org.apache.accumulo.core.client.security.tokens.PasswordToken;
 import org.apache.accumulo.core.util.shell.Shell;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -13,31 +9,25 @@ import org.apache.maven.plugin.MojoFailureException;
 /**
  * <pre>
  * Runs an Accumulo shell.
- *
+ * 
  * @goal shell
  * @requiresProject false
  */
-public class AccumuloShellMojo extends AbstractTestServerMojo
-		implements MojoMXBean {
+public class AccumuloShellMojo extends AbstractTestServerMojo implements MojoMXBean {
 
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
-        try {
-            Shell shell = new Shell();
-            shell.updateUser("root", new PasswordToken("password"));
-            shell.start();
-        } catch (IOException e) {
-            throw new MojoExecutionException("Error starting Accumulo shell", e);
-        } catch (AccumuloSecurityException e) {
-            throw new MojoExecutionException("Error starting Accumulo shell", e);
-        } catch (AccumuloException e) {
-            throw new MojoExecutionException("Error starting Accumulo shell", e);
-        }
-
+		try {
+			Shell shell = new Shell();
+			shell.config("-u", "root", "-p", "password", "-z", "accumulo", "localhost:2181");
+			shell.start();
+		} catch (IOException e) {
+			throw new MojoExecutionException("Error starting Accumulo shell", e);
+		}
 	}
 
-    @Override
-    public void shutdown() throws Exception {
+	@Override
+	public void shutdown() throws Exception {
 
-    }
+	}
 }
