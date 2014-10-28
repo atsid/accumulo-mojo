83f2ad7117c5c68e4cb3c23225d1f74612673489
Jon McLean <jon.mclean@atsid.com>
2014-10-28 12:03:17 -0700

    Forced stream redirection to stop before processes are killed.  In some situations we were getting StreamClosed exceptions when trying to read the InputStream after it was closed.  I think this was because the process was killed during an actual InputStream read.

--- accumulo-maven-plugin/src/main/java/com/atsid/runner/AbstractServerTestRunner.java
+++ accumulo-maven-plugin/src/main/java/com/atsid/runner/AbstractServerTestRunner.java
@@ -1,27 +1,39 @@
 package com.atsid.runner;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.lang.management.ManagementFactory;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.management.MBeanServer;
 import javax.management.ObjectName;
 
+import com.google.common.collect.Lists;
+import com.google.common.io.NullOutputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public abstract class AbstractServerTestRunner implements
-		ServerTestRunnerMXBean {
+public abstract class AbstractServerTestRunner implements ServerTestRunnerMXBean {
 
 	protected Logger logger = LoggerFactory.getLogger(this.getClass());
 	private boolean quiet;
 	protected Process process;
+	private final List<OutputRedirector> outputRedirectors = Lists.newLinkedList();
 
 	public void startupServer() throws Exception {
 
 		Runtime.getRuntime().addShutdownHook(new Thread() {
 			public void run() {
 				if (process != null) {
+					try {
+						stopOutputRedirection();
+					} catch (IOException | InterruptedException e) {
+						logger.error("Error stopping output stream redirection", e);
+					}
+
 					process.destroy();
 					logger.info("Shutdown hook called, child process has halted");
 				}
@@ -45,20 +57,31 @@ public abstract class AbstractServerTestRunner implements
 
 	protected abstract Process initServer() throws Exception;
 
+	/**
+	 * Stops all output redirection threads. This is to prevent reading from
+	 * closed streams.
+	 */
+	private void stopOutputRedirection() throws IOException, InterruptedException {
+		for (OutputRedirector outputRedirector : this.outputRedirectors) {
+			outputRedirector.stop();
+		}
+	}
+
 	public void registerAsMBean() throws Exception {
 		String className = this.getClass().getSimpleName();
 		MBeanServer mbeanServer = ManagementFactory.getPlatformMBeanServer();
-		ObjectName objectName = ObjectName.getInstance(String.format(
-				"%s:type=%s", getClass().getPackage().getName(), className));
+		ObjectName objectName = ObjectName.getInstance(String.format("%s:type=%s", getClass().getPackage().getName(),
+				className));
 		logger.info("Preparing to register as MBean");
 		mbeanServer.registerMBean(this, objectName);
-		logger.info("Started MBean service for "
-				+ this.getClass().getSimpleName());
+		logger.info("Started MBean service for " + this.getClass().getSimpleName());
 	}
 
 	public void shutdownServer() throws Exception {
 		if (process != null) {
 			logger.info("Shutting down " + this.getClass().getSimpleName());
+			stopOutputRedirection();
+
 			process.destroy();
 			process.waitFor();
 		} else {
@@ -66,41 +89,72 @@ public abstract class AbstractServerTestRunner implements
 		}
 	}
 
-	protected void redirectInputStream(InputStream source,
-			OutputStream destination) {
-		OutputRedirector outputRedirector = new OutputRedirector(source,
-				destination);
+	protected void redirectInputStream(InputStream source, OutputStream destination) {
+		OutputRedirector outputRedirector = new OutputRedirector(source, destination);
 		Thread runnerThread = new Thread(outputRedirector);
 		runnerThread.setDaemon(true);
 		runnerThread.start();
+
+		outputRedirectors.add(outputRedirector);
 	}
 
 	public static class OutputRedirector implements Runnable {
 		private InputStream inputStream;
 		private OutputStream outputStream;
+		private boolean continueOutputRedirection = true;
+		private Thread runningThread;
+		private static AtomicInteger redirectorId = new AtomicInteger(0);
+		private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-		public OutputRedirector(InputStream inputStream,
-				OutputStream outputStream) {
-			this.inputStream = inputStream;
-			this.outputStream = outputStream;
+		public OutputRedirector(InputStream inputStream, OutputStream outputStream) {
+			this.inputStream = new BufferedInputStream(inputStream);
+
+			if (outputStream != null) {
+				this.outputStream = outputStream;
+			} else {
+				this.outputStream = new NullOutputStream();
+			}
 		}
 
 		public void run() {
 			byte[] readBuffer = new byte[256];
+			Thread.currentThread().setName("OutputRedirector #" + Integer.toString(redirectorId.getAndIncrement()));
 			try {
-				while (true) {
-					int length = inputStream.read(readBuffer);
-					if (length == -1) {
-						break;
-					}
-					if (outputStream != null) {
+				this.runningThread = Thread.currentThread();
+
+				while (this.continueOutputRedirection) {
+					if (this.inputStream.available() > 0) {
+						// Read what is available (do not wait for EOF)
+						int length = inputStream.read(readBuffer);
+						if (length == -1) {
+							break;
+						}
+
 						outputStream.write(readBuffer, 0, length);
 						outputStream.flush();
+					} else {
+						// No data available. Wait a bit and try again.
+						Thread.sleep(100);
 					}
 				}
-			} catch (Exception e) {
-				throw new RuntimeException(e);
+			} catch (Throwable e) {
+				logger.error("Error redirecting input stream", e);
 			}
+		}
+
+		public void stop() throws InterruptedException, IOException {
+			this.continueOutputRedirection = false;
+
+			if (this.runningThread != null) {
+				this.runningThread.join(10000);
+			}
+
+			if (inputStream != null) {
+				this.inputStream.close();
+			}
+
+			// Do not close the output stream since it is System.out or
+			// System.err.
 		}
 	}
 
